1a35394ac7b436b36094151f2ce209effa6a0865
Shao-Tsu <shaotsu.chen@gmail.com>
2024-05-04 16:37:32 +0300

    added loop

--- dummy-data-generator.py
+++ dummy-data-generator.py
@@ -2,40 +2,48 @@ from faker import Faker
 import csv
 import random
 import os.path
+from datetime import date
 fake = Faker()
 
 
-## variable for columns
-date = fake.date_time()
-username = fake.user_name()
-brand = random.choice(['brand1','brand2','brand3','brand'])
-segment = random.choice(['Occasional','Potential','VIP','Inactive'])
-deposit_eur = random.randint(0,10000)
-revenue_eur = random.randint(-10000,10000)
-fee_eur = random.randint(0,100)
-cost_eur = random.randint(0,10000)
-is_active = random.choice(['TRUE', 'FALSE'])
+i = 0
+
+while i < 2:
+    ## variable for columns
+    transaction_date = fake.date_between(start_date=date(2023,12,1), end_date=date(2024,4,1))
+    username = fake.user_name()
+    brand = random.choice(['Brand 1','Brand 2','Brand 3','Brand 4'])
+    segment = random.choice(['Occasional','Potential','VIP','Inactive'])
+    deposit_eur = random.randint(0,1000)
+    revenue_eur = random.randint(-1000,1000)
+    fee_eur = random.randint(0,100)
+    cost_eur = random.randint(0,1000)
+    is_active = random.choice(['TRUE', 'FALSE'])
 
 
-## dummy dict
-users = {
-        'date': date,
-        'username': username, 
-        'brand': brand,
-        'segment': segment,
-        'deposit_eur': deposit_eur,
-        'revenue_eur': revenue_eur,
-        'cost_eur': cost_eur,
-        'fee_eur': fee_eur,
-        'is_active': is_active,
-         }
+    ## dummy dict
+    users = {
+            'transaction_date': transaction_date,
+            'username': username, 
+            'brand': brand,
+            'segment': segment,
+            'deposit_eur': deposit_eur,
+            'revenue_eur': revenue_eur,
+            'cost_eur': cost_eur,
+            'fee_eur': fee_eur,
+            'is_active': is_active,
+            }
 
-file_exists = os.path.isfile("users.csv")
+    file_exists = os.path.isfile("users.csv")
 
-with open('users.csv','a', newline='') as f:
-            fields = ['date', 'username', 'brand', 'segment', 'deposit_eur','revenue_eur','cost_eur','fee_eur','is_active']
+
+    with open('users.csv','a', newline='') as f:
+            fields = ['transaction_date', 'username', 'brand', 'segment', 'deposit_eur','revenue_eur','cost_eur','fee_eur','is_active']
             writer = csv.DictWriter(f, fieldnames=fields)
+
             if not file_exists:
                 writer.writeheader()
             else: 
-                writer.writerow(users)
+                writer.writerow(users)  
+
+i += 1
