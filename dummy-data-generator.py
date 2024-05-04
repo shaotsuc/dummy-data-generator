ecfde12fdc311a7e5c9f811c0c7f48ce173f9dea
Shao-Tsu <shaotsu.chen@gmail.com>
2024-05-04 15:45:47 +0300

    modified dictionary

--- dummy-data-generator.py
+++ dummy-data-generator.py
@@ -7,16 +7,16 @@ fake = Faker()
 ## variable for columns
 date = fake.date_time()
 username = fake.user_name()
-brand = random(['brand1,brand2,brand3,brand4'])
-segment = random(['Occasional','Potential','VIP','Inactive'])
+brand = random.choice(['brand1','brand2','brand3','brand'])
+segment = random.choice(['Occasional','Potential','VIP','Inactive'])
 deposit_eur = random.randint(0,10000)
 revenue_eur = random.randint(-10000,10000)
 fee_eur = random.randint(0,100)
 cost_eur = random.randint(0,10000)
-is_active = random('TRUE', 'FALSE')
+is_active = random.choice(['TRUE', 'FALSE'])
 
 
-## dummy data
+## dummy dict
 users = {
         'date': date,
         'username': username, 
@@ -29,9 +29,15 @@ users = {
         'is_active': is_active,
          }
 
+def generator():
+    with open('users.csv','w', newline='') as f:
+            fields = ['date', 'username', 'brand', 'segment', 'deposit_eur','revenue_eur','cost_eur','fee_eur','is_active']
+            writer = csv.DictWriter(f, fieldnames=fields)
+            writer.writeheader()
+            writer.writerow(users)
+        
 
-with open('users.csv','w', newline='') as csv_file:
-    fieldnames = ['date', 'username', 'brand', 'segment', 'deposit_eur','revenue_eur','cost_eur','fee_eur','is_active']
-    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-    writer.writeheader()
-    writer.writerow(users)
+i = 0
+while i < 10: 
+    generator()
+    i += 1
